5cd5b4e2289cd9d949358923cb3346dfe7a15ec3
chandrababu <kamanichandrababu@gmail.com>
2015-02-19 18:56:36 -0500

    final push of first file

--- rubypractice/first.rb
+++ rubypractice/first.rb
@@ -30,3 +30,54 @@ Say.good_night("babu")
 Say.new("ruby")
 Say.new("rails").good_morning("kamani")
 
+#Variables and access
+$global="chandra"
+class Outer
+@@classlevel="babu"
+def call
+@name="naidu"
+puts "inside calling #{@name}"
+anothercall
+end
+def anothercall
+puts $global+@@classlevel+@name
+end
+end
+
+Outer.new.call
+
+#Loops example
+
+def biz(number)
+puts "given number is #{number}"
+if(number%5==0 && number %3==0)
+  puts "bizzfuzz"
+elsif(number%5==0)
+ puts "bizz"
+elsif(number%3==0)
+ puts "fuzz"
+else
+ puts "nothing"
+end
+end
+
+biz(15)
+biz(20)
+biz(9)
+biz(8)
+
+
+puts 3+4
+
+puts 3+4
+
+5.times.each {|i| puts "iteration is #{i}" };1
+
+[1,2,3,4,5].each {|i| puts "value is #{i}"};1
+
+a=4
+while a<20
+a*=2
+puts a
+end
+
